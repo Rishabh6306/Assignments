ea84a4ec5a589c566bbf311c30df9b5e76c263da
Rishabh Srivastava <rishabhsrivastav722@gmail.com>
2023-03-13 16:46:25 -0400

    first origin

---
+++ Milestone -1 Assignment  02.03.2023/2. Calculator/calculator.js
@@ -0,0 +1,22 @@
+let num1 = 20;
+let num2 = 30;
+// let operator =  "+";
+
+switch (num1, num2, "+") {
+    case "+":
+     console.log(num1 + num2 );
+        break;
+    case "-":
+         console.log(num1 - num2 );
+        break;
+    case "*":
+         console.log(num1 * num2 );
+        break;
+    case "/":
+       console.log(num1 / num2 );
+        break;
+
+    default:
+        console.log("Invalid operator");
+        break;
+}
