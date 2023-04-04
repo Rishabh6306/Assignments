eafd49072cd45ac0823e39005775932e33ef9a2f
Rishabh Srivastava <rishabhsrivastav722@gmail.com>
2023-04-04 18:12:22 -0400

    3April Assignment Added

--- Milestone -1 Assignment  02.03.2023/2. Calculator/calculator.js
+++ Milestone -1 Assignment  02.03.2023/2. Calculator/calculator.js
@@ -1,8 +1,7 @@
 let num1 = 20;
 let num2 = 30;
-// let operator =  "+";
-
-switch (num1, num2, "+") {
+let operator = "+"
+switch (num1, num2, operator) {
     case "+":
      console.log(num1 + num2 );
         break;
